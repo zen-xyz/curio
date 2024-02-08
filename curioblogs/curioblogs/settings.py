f8f221e25f9b0306602388e845ec483a5c0d7055
Zen <114018545+zen-xyz@users.noreply.github.com>
2024-02-09 01:39:12 +0600

    Add files via upload

--- curioblogs/curioblogs/settings.py
+++ curioblogs/curioblogs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pa5$v(elh^&&dfjpnyb$mh6fsjn%dh1gren#=lv4q2$g#o3^+*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
